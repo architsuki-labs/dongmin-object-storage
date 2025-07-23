a4f8c390ff8134f4a2145b51db5d44540cf6af3d
dongminCheon <aehdals9900@gmail.com>
2025-07-23 22:01:16 +0900

    init

---
+++ src/main/kotlin/com/akitsuki/storage/DongminObjectStorageApplication.kt
@@ -0,0 +1,11 @@
+package com.akitsuki.storage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongminObjectStorageApplication
+
+fun main(args: Array<String>) {
+	runApplication<DongminObjectStorageApplication>(*args)
+}
---
+++ src/test/kotlin/com/akitsuki/storage/DongminObjectStorageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akitsuki.storage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DongminObjectStorageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
